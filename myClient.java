3cd18ec60c6945c5ea607c82d60d6ff18111f869
MendoMq <daniel.mendham@students.mq.edu.au>
2022-03-25 17:31:00 +1100

    Week 5 Job scheduling

--- myClient.java
+++ myClient.java
@@ -1,11 +1,14 @@
 import java.net.*;  
 import java.io.*;  
+import java.util.*;
+import java.lang.*;
 class MyClient{  
 	public static void main(String args[])throws Exception{  
 		Socket s=new Socket("localhost",50000);    
 		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
 		BufferedReader din=new BufferedReader(new InputStreamReader(s.getInputStream()));  
-     
+		String str="";
+
 		dout.write(("HELO\n").getBytes());  
 		dout.flush();  
 		
@@ -13,7 +16,7 @@ class MyClient{
 
 		dout.write(("AUTH root\n").getBytes());
        		dout.flush();
-
+		
 		System.out.println("Message: "+din.readLine());
 
 		dout.write(("REDY\n").getBytes());
@@ -21,12 +24,47 @@ class MyClient{
 
                 System.out.println("Message: "+din.readLine());
 
-		dout.write(("QUIT\n").getBytes());
+                dout.write(("GETS All\n").getBytes());
+                dout.flush();
+		
+		str = din.readLine();
+		System.out.println("Message : "+str);
+		String[] string = str.split(" ");
+		int num = Integer.parseInt(string[1]);
+
+		dout.write(("OK\n").getBytes());
 		dout.flush();
 
+		int maxCore=0;
+		String type="";
+
+		for(int i=0;i<num;i++){
+			str = din.readLine();
+			string = str.split(" ");
+			if (Integer.parseInt(string[4]) > maxCore) type=string[0];
+			System.out.println("DATA: "+str);
+		}
+
+		System.out.println("TYPE: "+type);
+
+		dout.write(("OK\n").getBytes());
+		dout.flush();
+		System.out.println("Message: "+din.readLine());
+		
+		dout.write(("SCHD 0 "+ type +" 0\n").getBytes());
+		dout.flush();
+		
+		System.out.println("Message: "+din.readLine());
+
+		dout.write(("OK\n").getBytes());
+                dout.flush();
+
                 System.out.println("Message: "+din.readLine());
+
+		dout.write(("QUIT\n").getBytes());
+		dout.flush();
 
 		dout.close();  
 		s.close();  
 	}	
-}  
+}
