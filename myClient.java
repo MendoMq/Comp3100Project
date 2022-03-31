eae7aa5655a04242c9c0a348efbc3747a58fa9f6
Daniel Mendham <daniel.mendham@students.mq.edu.au>
2022-03-31 17:54:40 +1100

    JOB Scheduling repeater complete

--- myClient.java
+++ myClient.java
@@ -36,31 +36,66 @@ class MyClient{
 		dout.flush();
 
 		int maxCore=0;
+		int typeNum=0;
 		String type="";
 
 		for(int i=0;i<num;i++){
 			str = din.readLine();
 			string = str.split(" ");
-			if (Integer.parseInt(string[4]) > maxCore) type=string[0];
+			if (Integer.parseInt(string[4]) > maxCore){
+			       	maxCore=Integer.parseInt(string[4]);
+				type=string[0];
+				typeNum=0;
+			}
+			if (string[0].equals(type)){
+				typeNum++;
+			}
 			System.out.println("DATA: "+str);
 		}
-
+		System.out.println("MAXCORE "+maxCore);
 		System.out.println("TYPE: "+type);
+		System.out.println("TYPENUM: "+typeNum);
 
 		dout.write(("OK\n").getBytes());
 		dout.flush();
-		System.out.println("Message: "+din.readLine());
-		
-		dout.write(("SCHD 0 "+ type +" 0\n").getBytes());
-		dout.flush();
 		
 		System.out.println("Message: "+din.readLine());
-
-		dout.write(("OK\n").getBytes());
+		
+		dout.write(("REDY\n").getBytes());
                 dout.flush();
+	
+		int newJobID=0;
+		str=din.readLine();
+		string=str.split(" ");
+		while (string[0].equals("JOBN")){
+		       	newJobID = Integer.parseInt(string[2]); 
+	                System.out.println("Message: "+str);
 
-                System.out.println("Message: "+din.readLine());
+			dout.write(("SCHD "+ newJobID  +" "+ type +" 0\n").getBytes());
+			dout.flush();
+		
+			System.out.println("Message: "+din.readLine());
 
+			//dout.write(("OK\n").getBytes());
+			//dout.flush();
+
+                	//System.out.println("Message: "+din.readLine());
+		
+			dout.write(("REDY\n").getBytes());
+	                dout.flush();
+
+                        str=din.readLine();
+                	string=str.split(" "); 
+			System.out.println("Message: "+str);
+			
+			while (string[0].equals("JCPL")){
+                        	dout.write(("REDY\n").getBytes());
+                        	dout.flush();
+
+                        	str=din.readLine();
+                        	string=str.split(" ");
+                	}
+		}
 		dout.write(("QUIT\n").getBytes());
 		dout.flush();
 
